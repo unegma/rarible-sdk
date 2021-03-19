f0fc44821bc6bbd14e45540c2eaf4a7b94582cfe
Timothy Coleman <timhc22@gmail.com>
2021-03-19 23:55:08 +0000

    Add mintNFT placeholder function

--- src/RaribleSDK.js
+++ src/RaribleSDK.js
@@ -16,6 +16,21 @@ class RaribleSDK {
   constructor(network= "mainnet") {
     this.network = network;
   }
+
+  /**
+   * @typedef { "ERC1155" | "ERC721" } type
+   */
+
+  /**
+   *
+   * @param {Object} raribleOptions
+   * @param {type} type
+   * @returns {Promise<{data: string}>}
+   */
+  async mintNFT(raribleOptions, type) {
+
+    return { data: "Minted!" }
+  }
 }
 
 export default RaribleSDK;
